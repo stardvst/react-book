7d0ad4664b75f881c13ae356b8fdfe936bfe2382
stardvst <lilia.manukyan@hotmail.com>
2022-07-25 08:53:53 +0400

    added useEffect example

---
+++ src/components/Checkebox.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+
+const Checkebox = () => {
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => alert(`checked: ${checked}`));
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        name="check"
+        id="check_box"
+        value={checked}
+        onChange={() => setChecked(checked => !checked)}
+      />
+      {checked ? 'checked' : 'unchecked'}
+    </>
+  );
+};
+
+export default Checkebox;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,10 +2,12 @@ import { FaTrash } from 'react-icons/fa';
 import Instructions from './Instructions';
 import Ingredient from './Ingredient';
 import StarRating from './StarRating';
+import Checkebox from './Checkebox';
 
 const Recipe = ({ id, name, ingredients, steps, rating, onRemove, onRate }) => {
   return (
     <section id="baked-salmon">
+      <Checkebox />
       <h1>{name}</h1>
       <ul className="ingredients">
         {ingredients.map((ingredient, i) => (
