8312d6b117701e0098b5a6313e2eb80b0097527b
stardvst <lilia.manukyan@hotmail.com>
2022-07-29 08:57:35 +0400

    use useReducer for more compact state updating

--- src/components/Checkebox.js
+++ src/components/Checkebox.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 // import { useEffect } from 'react';
 
 const Checkebox = () => {
-  const [checked, setChecked] = useState(false);
+  const [checked, toggle] = useReducer(checked => !checked, false);
 
   //useEffect(() => alert(`checked: ${checked}`));
+
+ // const toggle = () => setChecked(checked => !checked);
 
   return (
     <>
@@ -13,7 +15,7 @@ const Checkebox = () => {
         name="check"
         id="check_box"
         value={checked}
-        onChange={() => setChecked(checked => !checked)}
+        onChange={toggle}
       />
       {checked ? 'checked' : 'unchecked'}
     </>
