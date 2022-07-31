07472902bd4b6f06354a3090521ead8ab568c672
stardvst <lilia.manukyan@hotmail.com>
2022-07-31 09:20:40 +0400

    added saving github user info in localStorage

--- src/components/GithubUser.js
+++ src/components/GithubUser.js
@@ -1,16 +1,32 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
+
 const GithubUser = ({ login }) => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login && loadJSON(`user:${login}`)) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location,
+    });
+  }, [data, login]);
 
   useEffect(() => {
     if (!login) return;
+    if (data && data.login === login) return;
     fetch(`https://api.github.com/users/${login}`)
       .then(res => res.json())
       .then(setData)
       .catch(console.error);
-  }, [login]);
+  }, [login, data]);
 
   if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
 
