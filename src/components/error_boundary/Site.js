cdcdcbd5bc9b9f391414007a0e2b8b2746933b6d
stardvst <lilia.manukyan@hotmail.com>
2022-08-13 16:24:25 +0400

    added suspense and error boundary

--- src/components/error_boundary/Site.js
+++ src/components/error_boundary/Site.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
+import { ClimbingBoxLoader } from 'react-spinners';
 import Agreement from './Agreement';
-import Main from './Main';
+import ErrorBoundary from './ErrorBoundary';
+
+const Main = React.lazy(() => import('./Main'));
 
 const Site = () => {
   const [agree, setAgree] = useState(false);
   if (!agree) return <Agreement onAgree={() => setAgree(true)} />;
-  return <Main />;
+  return (
+    <ErrorBoundary>
+      <Suspense fallback={<ClimbingBoxLoader />}>
+        <Main />
+      </Suspense>
+    </ErrorBoundary>
+  );
 };
 
 export default Site;
